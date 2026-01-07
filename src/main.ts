525009d3b1d86f8021fd08e1f339802f817feae2
ry023 <ry023.blue@gmail.com>
2026-01-07 10:55:34 +0900

    slack
    fetch

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+//import {WebClient} from '@slack/web-api'
 
 /**
  * The main function for the action.
@@ -36,6 +37,25 @@ export async function run(): Promise<void> {
 
     core.info(`Comment body: ${comment.data.body}`)
     core.info(`Comment body_html: ${comment.data.body_html}`)
+
+    const slackToken = core.getInput('slack-token')
+    const slackChannel = core.getInput('slack-channel')
+    //const slackClient = new WebClient(slackToken)
+    //await slackClient.chat.postMessage({
+    //  channel: slackChannel,
+    //  text: `New comment on issue #${issueNumber} by ${sender}`
+    //})
+    await fetch("https://slack.com/api/chat.postMessage", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${slackToken}`,
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        channel: slackChannel,
+        text: "CI finished ✅",
+      }),
+    });
   } catch (error) {
     core.setFailed((error as Error).message)
   }
